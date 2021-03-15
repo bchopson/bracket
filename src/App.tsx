c96d9a3b5f688b941b8cb211778119862a776713
Ben Chopson <bchopson@users.noreply.github.com>
2021-03-14 23:13:02 -0400

    Get bracket-data and spit out brackets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import ncaam2021 from "bracket-data/browser/ncaam-2021";
+
 import Game from "./components/Game";
 
 function App() {
+  const order = ncaam2021.order;
+
   return (
     <div className="container mx-auto">
-      <Game
-        home={{ name: "Butler", ranking: 11 }}
-        away={{ name: "Duke", ranking: 2 }}
-      />
+      {ncaam2021.constants.REGION_IDS.map((regionId: string) => {
+        const region = ncaam2021.bracket.regions[regionId];
+        return order.reduce(
+          (components: JSX.Element[], ranking: number, i: number) => {
+            if (i % 2 === 0) {
+              const homeRanking = ranking;
+              const awayRanking = order[i + 1];
+              const homeTeam = {
+                ranking: homeRanking,
+                name: region.teams[homeRanking - 1],
+              };
+              const awayTeam = {
+                ranking: awayRanking,
+                name: region.teams[awayRanking - 1],
+              };
+              const key = `${regionId}${homeRanking}${awayRanking}`;
+              components.push(
+                <Game key={key} home={homeTeam} away={awayTeam} />
+              );
+            }
+            return components;
+          },
+          []
+        );
+      })}
     </div>
   );
 }
---
+++ src/bracket-data.d.ts
@@ -0,0 +1,4 @@
+declare module "bracket-data/browser/ncaam-2021" {
+  const ncaam2021: any;
+  export default ncaam2021;
+}
